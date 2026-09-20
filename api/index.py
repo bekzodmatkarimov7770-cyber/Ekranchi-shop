8a7767e6211fe18ef5e906588d57ae5a2ca91948
bekzodmatkarimov7770-cyber <bekzodmatkarimov7770@gmail.com>
2026-09-21 01:20:22 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, Inlin
 import json, base64, os
 
 BOT_TOKEN = "8484579263:AAGZu38VEXN4Hx5Yup12JoZnBZa1TlCDVy0"
-WEB_APP_URL = "https://bekzodmatkarimov7770-cyber.github.io/Ekranchi-shop/market.html?v=pro_v50"
+WEB_APP_URL = "https://bekzodmatkarimov7770-cyber.github.io/Ekranchi-shop/market.html?v=pro_v51"
 ADMIN_ID = 1758833704
 CARD_NUMBER = "9860 1266 0304 4796"
 CARD_NAME = "Bekzod M. (Humo / Uzcard)"
